6b127cecb83a6dea27dd666fd33099e88e78c0f2
shaniba17 <shanibatm2002@gmail.com>
2024-06-13 06:17:43 +0530

    App added

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddQuote from './AddQuote'
+import SearchQuote from './SearchQuote'
+
+const App = () => {
+  return (
+    <div>
+        <AddQuote/>
+        <SearchQuote/>
+    </div>
+  )
+}
+
+export default App
